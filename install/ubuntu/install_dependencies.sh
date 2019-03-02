eb9e2bc3b5001ab04aa95ca348491b277f33892a
nexayq <nexayq@users.noreply.github.com>
2019-03-02 02:13:42 +0100

    Install - Stop when exit occurs

--- install/ubuntu/install_dependencies.sh
+++ install/ubuntu/install_dependencies.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-sudo apt install python-pip
+# stop script when exit occurs
+set -e
+
+# OpenCV
 sudo apt install python-numpy
 sudo apt install python-opencv
 
+# PyQt
 sudo apt install python-qt4
 
+# PyAutoGUI
+sudo apt install python-pip
 pip install pyautogui --user
+
 
 # run application
 ../../color_picker/run.sh
