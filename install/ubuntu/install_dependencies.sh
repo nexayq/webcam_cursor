7e687c0daf499d4b954ebc95a356ee0516831f8a
nexayq <nexayq@users.noreply.github.com>
2019-03-02 01:46:00 +0100

    Install - Ubuntu dependencies

---
+++ install/ubuntu/install_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sudo apt install python-pip
+sudo apt install python-numpy
+sudo apt install python-opencv
+
+sudo apt install python-qt4
+
+pip install pyautogui --user
+
+# run application
+../../color_picker/run.sh
