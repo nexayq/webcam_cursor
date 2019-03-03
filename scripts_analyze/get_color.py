682d3d9770385f330cbf3a4a76d826dfc8c6a090
nexayq <nexayq@users.noreply.github.com>
2019-03-03 12:49:41 +0100

    Python - get_color put import on top

--- scripts_analyze/get_color.py
+++ scripts_analyze/get_color.py
@@ -3,9 +3,9 @@ import time
 import pyautogui
 import cv2
 import numpy as np
+import gtk # python-gtk2
 
 def get_pixel_colour(i_x, i_y):
-        import gtk # python-gtk2
         o_gdk_pixbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, 1, 1)
         o_gdk_pixbuf.get_from_drawable(gtk.gdk.get_default_root_window(), gtk.gdk.colormap_get_system(), i_x, i_y, 0, 0, 1, 1)
         return tuple(o_gdk_pixbuf.get_pixels_array().tolist()[0][0])
