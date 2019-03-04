5afb91fbb393e595582dd0975fd3774686b7e93f
nexayq <nexayq@users.noreply.github.com>
2019-03-04 16:57:40 +0100

    Scripts - get_color to support Windows also - Qt version

---
+++ scripts/get_color_qt.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+import time
+import pyautogui
+import cv2
+import numpy as np
+#  import gtk # python-gtk2
+import PyQt4.QtGui # python-qt4
+
+def get_pixel_colour(i_x, i_y):
+        app = PyQt4.QtGui.QApplication([])
+        long_qdesktop_id = PyQt4.QtGui.QApplication.desktop().winId()
+        long_colour = PyQt4.QtGui.QPixmap.grabWindow(long_qdesktop_id, i_x, i_y, 1, 1).toImage().pixel(0, 0)
+        i_colour = int(long_colour)
+        return ((i_colour >> 16) & 0xff), ((i_colour >> 8) & 0xff), (i_colour & 0xff)
+
+#  print get_pixel_colour(0, 0)
+
+while(1):
+    x, y = pyautogui.position()
+    #  print get_pixel_colour(100, 100)
+    print "Cursor:", x, y
+    rgb = get_pixel_colour(x, y)
+    bgr = rgb[2], rgb[1], rgb[0]
+    #  hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+    hsv = cv2.cvtColor(np.uint8([[bgr]]),cv2.COLOR_BGR2HSV)[0][0]
+    print "BGR:", bgr
+    print "HSV:", hsv
+    time.sleep(1)
