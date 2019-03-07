1783cb4d18cf44627ad795fdbcad891ae47ec725
nexayq <nexayq@users.noreply.github.com>
2019-03-07 19:24:09 +0100

    Install - Dependencies for ArUco

--- install/ubuntu/install_dependencies.sh
+++ install/ubuntu/install_dependencies.sh
@@ -13,4 +13,8 @@ sudo apt install python-qt4
 # PyAutoGUI
 sudo apt install python-pip
 sudo apt install python-xlib
-pip install pyautogui --user
+pip2 install pyautogui --user
+
+# ArUco
+sudo apt install libeigen3-dev
+pip2 install aruco --user
