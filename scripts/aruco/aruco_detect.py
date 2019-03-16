75d5b151b272c3aa0cd4effedd41d9f609df5dbf
nexayq <nexayq@users.noreply.github.com>
2019-03-16 12:02:47 +0100

    Scripts - Try to analyze grayscale data - reduce CPU usage

--- scripts/aruco/aruco_detect.py
+++ scripts/aruco/aruco_detect.py
@@ -4,10 +4,16 @@ from __future__ import print_function, division
 import cv2
 from cv2 import aruco
 import numpy as np
+import time
 
+
+image_data = np.zeros([480, 640])
 
 # get video input from web camera
 cap = cv2.VideoCapture(0)
+
+cap.set(cv2.CAP_PROP_CONVERT_RGB, False)
+print( cap.get(cv2.CAP_PROP_CONVERT_RGB) )
 #  print(cap.get(3))
 #  cap.set(3,1280)
 #  cap.set(4,720)
@@ -29,42 +35,48 @@ cap = cv2.VideoCapture(0)
 while True:
         _,frame = cap.read()
         #  print(frame)
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #  gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        gray = frame[:,:,0].astype(np.uint8)
+
         aruco_dict = aruco.Dictionary_get(aruco.DICT_4X4_250)
         parameters =  aruco.DetectorParameters_create()
         corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
-        frame_markers = aruco.drawDetectedMarkers(frame.copy(), corners, ids)
+        #  frame_markers = gray
+        #  frame_markers = aruco.drawDetectedMarkers(gray.copy(), corners, ids)
         #  gray_markers = aruco.drawDetectedMarkers(gray.copy(), corners, ids)
 
-        if ids is not None:
-            for i in range(len(ids)):
-                c = corners[i]
-                #  print("c[" + str(i) + "] = " + str(c[i]))
-                #  print("c[" + str(i) + "] = " + str(c[i]))
-                print("x = " + str(c[i][0][0]))
-                print("y = " + str(c[i][0][1]))
-                print("")
+        #  if ids is not None:
+            #  for i in range(len(ids)):
+                #  c = corners[i]
+                #  #  print("c[" + str(i) + "] = " + str(c[i]))
+                #  #  print("c[" + str(i) + "] = " + str(c[i]))
+                #  print("x = " + str(c[i][0][0]))
+                #  print("y = " + str(c[i][0][1]))
+                #  print("")
 
         #  frame_markers = frame
-        if rejectedImgPoints:
-            print(rejectedImgPoints)
-            print(rejectedImgPoints[0][0][0])
-            print(int(rejectedImgPoints[0][0][0][0]))
-            print()
+        #  if rejectedImgPoints:
+            #  print(rejectedImgPoints)
+            #  print(rejectedImgPoints[0][0][0])
+            #  print(int(rejectedImgPoints[0][0][0][0]))
+            #  print()
 
-            x1 = int(rejectedImgPoints[0][0][0][0])
-            y1 = int(rejectedImgPoints[0][0][0][1])
+            #  x1 = int(rejectedImgPoints[0][0][0][0])
+            #  y1 = int(rejectedImgPoints[0][0][0][1])
 
-            x2 = x1 + 30
-            y2 = y1 + 30
+            #  x2 = x1 + 30
+            #  y2 = y1 + 30
 
-            #  frame_markers = cv2.rectangle(frame,(384,0),(510,128),(0,255,0),3)
-            frame_markers = cv2.rectangle(frame_markers,(x1,y1),(x2,y2),(0,255,0),3)
-        else:
-            frame_markers = frame
+            #  #  frame_markers = cv2.rectangle(frame,(384,0),(510,128),(0,255,0),3)
+            #  frame_markers = cv2.rectangle(frame_markers,(x1,y1),(x2,y2),(0,255,0),3)
+        #  else:
+            #  frame_markers = frame
 
-        cv2.imshow("Web cam input", frame_markers)
+        cv2.imshow("Web cam input", gray)
         #  cv2.imshow("Gray input", gray_markers)
+
+        time.sleep(0.01)
 
         key = cv2.waitKey(1)
         #  if key == 27:
--- scripts/test_camera.py
+++ scripts/test_camera.py
@@ -2,17 +2,52 @@
 
 import cv2
 import numpy as np
+import time
+
+#  np.set_printoptions(threshold=np.inf)
+data = np.zeros([480, 640])
 
 # get video input from web camera
 cap = cv2.VideoCapture(0)
+#  cap.set(cv2.CAP_PROP_MODE, 2)
+#  cap.set(cv2.CAP_PROP_MODE, cv2.CAP_MODE_YUYV)
+cap.set(cv2.CAP_PROP_CONVERT_RGB, False)
+print( cap.get(cv2.CAP_PROP_CONVERT_RGB) )
 #  cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH,1280)
 #  cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT,720)
 
 while True:
         _,frame = cap.read()
+        #  frame = cv2.GrabFrame(cap)
+        #  frame = cv2.grab(cap)
         #  print(frame)
 
-        cv2.imshow("Web cam input", frame)
+        #  print(frame)
+        #  print(frame.shape)
+        #  print(frame._)
+
+        #  image = cv2.cvtColor(frame, cv2.COLOR_YUYV2GRAY)
+        #  image = frame[0][0][0]
+        #  image = frame(:,:,0)
+        #  for i in range(0,480):
+            #  for j in range(0,640):
+                #  #  print(i,j,frame[i,j,0])
+                #  data[i,j] = int(frame[i,j,0])
+        image = frame[:,:,0].astype(np.uint8)
+        print()
+        print(frame.shape)
+        #  print(image.shape)
+        print(image)
+        print()
+        print()
+        #  gray = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+
+        cv2.imshow("Web cam input", image)
+        #  cv2.imshow('im', cv2.imdecode(cap,-1))
+        #  cv2.imshow('im', cv2.imdecode(image,-1))
+        #  time.sleep(0.1)
+        time.sleep(0.01)
+        #  time.sleep(2)
 
         key = cv2.waitKey(1)
         #  if key == 27:
